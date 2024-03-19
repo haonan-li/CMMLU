5d9bb456daedc3a24a610434644f87785a2f6f4e
Haonan Li <nathan.8270.n@gmail.com>
2024-03-19 11:19:10 +0400

    clean pci code

--- src/run_pci_eval.py
+++ src/run_pci_eval.py
@@ -1,16 +1,230 @@
 import os
-import argparse
+import re
 import time
 import json
+import asyncio
+import logging
+import requests
+import csv
+import argparse
+import pandas as pd
 from tqdm import tqdm
 from collections import defaultdict
-import pandas as pd
-import csv
 
-from pci_utils import load_json, save_json,  extract_choice
-from pci_model import LLM
 from categories import name_en2zh, subcategories, categories
 from mp_utils import format_example, gen_prompt
+
+
+pci_url = "http://123.249.36.167:19203"
+
+def pci_generate(usr_contents, model_name=None, stream=False, model_key=None,
+                 max_length=4096, top_p=0.8, top_k=30, repetition_penalty=1.2, temperature=0.05):
+    """PCI TransGPT
+    url: 地址
+    usr_contents: 用户输入
+    """
+    # URI处理
+    if stream:
+        url_tail = "/api/gpt/chat/completions/stream"
+    else:
+        url_tail = "/api/gpt/chat/completions"
+
+    if model_key is None:
+        model_key = pci_url
+
+    url = model_key + url_tail
+
+    # Message处理
+    messages = []
+
+    # 用户输入
+    if isinstance(usr_contents, str):
+        messages.append({"role": "user", "content": usr_contents})
+    else:
+        raise ValueError(f"Contents type error for single turn chat, "
+                         f"user:{type(usr_contents)}, assistant: {type(asis_contents)}")
+
+    # body处理
+    body = {
+        "model": "PCI-TransGPT",
+        "max_tokens": max_length,
+        "top_p": top_p,
+        "temperature": temperature,
+        "top_k": top_k,
+        "repetition_penalty": repetition_penalty,
+        "messages": messages
+    }
+
+    # header处理
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    # 设置10次重连
+    retry_counter = 0
+    max_retries = 10
+    delay = 15  #
+
+    while retry_counter < max_retries:
+        try:
+            response = requests.post(url=url, json=body, headers=headers)
+            response_dict = response.json()
+
+            if "choices" in response_dict:
+                return response_dict['choices'][0]['message']['content']
+            else:
+                raise f"PCI generate failure: {str(response_dict)}"
+        except Exception as error:
+            retry_counter += 1
+            logging.warning(f"PCI generate failure: {repr(error)}")
+            if retry_counter <= max_retries:
+                logging.warning(f"Waiting for retry: {retry_counter} ...")
+                time.sleep(delay)
+            else:
+                logging.warning(f"Exceed maximum retries number!")
+                raise error
+
+
+class LLM:
+    def __init__(self, model_name, model_key, **kwargs):
+        self.model_name = model_name
+        self.model_key = model_key
+        self.kwargs = kwargs
+        self.usr_contents = None
+
+    def run(self, usr_contents):
+        self.usr_contents = usr_contents
+        return self.call_llm()
+
+    def get_model_type(self):
+        model_type = {
+            **{k: "pci" for k in ["pci_transgpt"] },
+        }
+        if self.model_name in model_type:
+            return model_type.get(self.model_name)
+        raise ValueError(f"LLM model name not supported: '{self.model_name}'")
+
+    def call_llm(self):
+        generate_funcs = {
+            "pci": pci_generate,
+        }
+
+        model_type = self.get_model_type()
+        generate_func = generate_funcs.get(model_type)
+        if generate_func:
+            self.kwargs.update({
+                'usr_contents': self.usr_contents,
+                'model_name': self.model_name,
+                'model_key': self.model_key
+            })
+
+            response = generate_func(**self.kwargs)
+            return response
+
+        raise KeyError(f"LLM model type not supported: '{model_type}'")
+
+
+class ParseError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+def load_json(json_file, multi=True):
+    if not os.path.exists(json_file):
+        raise FileNotFoundError("json file not found: {}".format(json_file))
+    if multi:
+        with open(json_file, encoding='utf-8', mode='r') as f:
+            data = [json.loads(line) for line in f]
+        return data
+    else:
+        with open(json_file, encoding='utf-8', mode='r') as f:
+            data = json.load(f)
+        return data
+
+def save_json(json_data, json_file, multi=True, indent=None):
+    if multi:
+        with open(json_file, encoding='utf-8', mode='w') as f:
+            for item in json_data:
+                json.dump(item, f, ensure_ascii=False, indent=indent)
+                f.write('\n')
+    else:
+        with open(json_file, encoding='utf-8', mode='w') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=indent)
+
+def extract_choice(response, choice_desc):
+    # choice_desc: 选项信息，包括选项字母和描述
+    if choice_desc is not None:
+        ref_letters = [c[0] for c in choice_desc]
+        ref_desc = [c[2:] for c in choice_desc]
+    else:
+        ref_letters = ["A", "B", "C", "D"]
+        ref_desc = None
+    if len(response) == 0:
+        return None
+    if ref_desc is not None:
+        pattern = '|'.join(re.escape(desc) for desc in ref_desc)
+        match = re.findall(pattern, response)
+        if len(set(match)) == 1:
+            return ref_letters[ref_desc.index(match[0])]
+    # 关键字匹配
+    # 1. Single match
+    patterns = [
+            (r'答案(选项)?(是|为)：? ?([A-Z]+)', 3),
+            (r'答案(是|为)选项 ?([A-Z]+)', 2),
+            (r'故?选择?：? ?([A-Z]+)', 1),
+            (r'([A-Z]+) ?选?项(是|为)?正确', 1),
+            (r'正确的?选项(是|为) ?([A-Z]+)', 2),
+            (r'答案(应该)?(是|为)([A-Z]+)', 3),
+            (r'选项 ?([A-Z]+) ?(是|为)?正确', 1),
+            (r'选择答案 ?([A-Z]+)', 1),
+            (r'答案?：?([A-Z]+)', 1),
+            (r'([A-Z]+)(选?项)?是?符合题意', 1),
+            (r'答案选项：? ?([A-Z]+)', 1),  # chatglm
+            (r'答案(选项)?为(.*?)([A-Z]+)', 3),  # chatgpt
+    ]
+    for pattern, idx in patterns:
+        m = re.search(pattern, response, re.M)
+        if m:
+            answer = m.group(idx)
+            return answer
+    # 2. Recursive match
+    patterns = [
+            (r'([A-Z]+)(.*?)当选', 1),
+            (r'([A-Z]+)(.*?)正确', 1),
+            (r'(综上)(.*?)([A-Z]+)(.*?)最', 3),
+            (r'(因此)(.*?)([A-Z]+)(.*?)最', 3),
+    ]
+    for pattern, idx in patterns:
+        m = re.search(pattern, response, re.M)
+        if m:
+            while m:
+                answer = m.group(idx)
+                m = re.search(pattern, m.group(0)[1:], re.M)
+            return answer
+    # 3. Weak single match
+    patterns = [
+            (r'[^不]是：? ?([A-Z]+)', 1),
+    ]
+    for pattern, idx in patterns:
+        m = re.search(pattern, response, re.M)
+        if m:
+            answer = m.group(idx)
+            return answer
+    # 4. Check the only mentioend choices
+    pattern = r'^[^ABCD]*([ABCD])[^ABCD]*$'
+    m = re.match(pattern, response)
+    if m:
+        answer = m.group(1)
+        return answer
+    # 最后
+    pattern = re.compile(r'[A-Z]+')
+    answer = pattern.findall(response)
+    if len(set(answer)) > 0:
+        return list(set(answer))[0]
+    return None
 
 def create_or_load_data(data_dir, model_name, file_name):
     data_dir = os.path.join(data_dir, model_name)
